267b0b6d2b394adbd7b083796582ecf820ebfee7
Mikael Juntti <juntti.mikael@gmail.com>
2016-08-09 14:06:42 +0200

    added date to posts
    the_date()

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ if (have_posts()) :
 	while (have_posts()) : the_post(); ?>
 	
 	<h2><?php the_title(); ?></h2>
+	<span class="post-date"><?php the_date(); ?></span>
 	<?php the_content(); ?>
 	<?php endwhile;
 	
